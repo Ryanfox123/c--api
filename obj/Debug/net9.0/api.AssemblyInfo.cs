0fa31fe1c2555acea7a43b24a61c95cbfb0b266e
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-10-07 18:31:34 +0100

    models created

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384a28b113808abd8d4d7ce994b41a3b259698e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
