deb0ebad794b03d95f67f5264bc418840f616ae9
Ryan Fox <ryanfox1@hotmail.co.uk>
2025-10-30 20:49:16 +0000

    cascading delete for stocks and their comments

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -18,7 +18,6 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
-
         public DbSet<Portfolio> Portfolios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -28,32 +27,37 @@ namespace api.Data
             builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId }));
 
             builder.Entity<Portfolio>()
-            .HasOne(u => u.AppUser)
-            .WithMany(u => u.Portfolios)
-            .HasForeignKey(p => p.AppUserId);
+                .HasOne(u => u.AppUser)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.AppUserId);
 
             builder.Entity<Portfolio>()
-            .HasOne(u => u.Stock)
-            .WithMany(u => u.Portfolios)
-            .HasForeignKey(p => p.StockId);
+                .HasOne(u => u.Stock)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.StockId);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.Stock)
+                .WithMany(s => s.Comments)
+                .HasForeignKey(c => c.StockId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             List<IdentityRole> roles = new List<IdentityRole>
-            {
-                new IdentityRole
-                {
-                    Id = "1",
-                    Name = "Admin",
-                    NormalizedName = "ADMIN",
-                },
-                new IdentityRole
-                {
-                    Id = "2",
-                    Name = "User",
-                    NormalizedName = "USER",
-                },
-            };
+    {
+        new IdentityRole
+        {
+            Id = "1",
+            Name = "Admin",
+            NormalizedName = "ADMIN",
+        },
+        new IdentityRole
+        {
+            Id = "2",
+            Name = "User",
+            NormalizedName = "USER",
+        },
+    };
             builder.Entity<IdentityRole>().HasData(roles);
         }
-
     }
 }
